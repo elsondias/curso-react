3e4bdd6af8ba29847e8daeebb4f9bf4339d876e1
Elson Alacoque <102491017+elsondias@users.noreply.github.com>
2025-08-28 23:09:01 -0300

    aula-03

---
+++ aula-03/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* 1 - IMAGEM EM PUBLIC */}
+      <img src="img.jpg" alt="rua" />
+    </div>
+  );
+}
+
+export default App;
