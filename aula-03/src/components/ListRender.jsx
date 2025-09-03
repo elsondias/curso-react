4b1492d869d430ef0944dbf1f7de6b94202eb1ea
Elson Alacoque <102491017+elsondias@users.noreply.github.com>
2025-09-02 22:28:51 -0300

    key-render

--- aula-03/src/components/ListRender.jsx
+++ aula-03/src/components/ListRender.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 
 function ListRender() {
-    const [list] = useState(["Goku", "Gohan", "Vegeta", "Piccolo"]);
+  const [list] = useState(["Goku", "Gohan", "Vegeta", "Piccolo"]);
 
-  return <div>
-    <ul>
+  const [users, setUsers] = useState([
+    { id: 1, name: "Goku", age: 30 },
+    { id: 2, name: "Vegeta", age: 31 },
+    { id: 3, name: "Gohan", age: 10 },
+    { id: 4, name: "Piccolo", age: 140 },
+  ]);
+
+  return (
+    <div>
+      {/* 4 - RENDERIZAÇÃO SEM KEY */}
+      <ul>
         {list.map((item) => (
-            <li>{item}</li>
+          <li>{item}</li>
         ))}
-    </ul>
-  </div>;
+      </ul>
+      {/* 5 - RENDERIZAÇÃO COM KEY*/}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListRender;
